76b8447d1ea742de4d3c6d724a52839a6da63dd2
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-06 16:31:29 -0300

    Fix Optional

--- workspace/01-springboot-servicio-productos/src/main/java/com/springboot/app/productos/models/service/ProductoServiceImpl.java
+++ workspace/01-springboot-servicio-productos/src/main/java/com/springboot/app/productos/models/service/ProductoServiceImpl.java
@@ -1,7 +1,6 @@
 package com.springboot.app.productos.models.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
