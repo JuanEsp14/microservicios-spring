c4caacfb93c091d449b5a968dc04660499ed538b
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-08 11:46:39 -0300

    Se crea el mircoservicio Item con los modelos Producto e Item

---
+++ workspace/01-springboot-servicio-item/src/main/java/com/springboot/app/item/models/Producto.java
@@ -0,0 +1,36 @@
+package com.springboot.app.item.models;
+
+import java.util.Date;
+
+public class Producto {
+
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private Date creadoEl;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreadoEl() {
+		return creadoEl;
+	}
+	public void setCreadoEl(Date creadoEl) {
+		this.creadoEl = creadoEl;
+	}
+}
