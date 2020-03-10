b2b79de4009efc13137a93e6ccfdcdaab7071ed4
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-10 10:51:29 -0300

    Se configura la dependencia Eureka en el proyecto producto

--- workspace/springboot-servicio-productos/src/main/java/com/springboot/app/productos/Application.java
+++ workspace/springboot-servicio-productos/src/main/java/com/springboot/app/productos/Application.java
@@ -2,8 +2,11 @@ package com.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//Habilitamos el cliente de Eureka en la app
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
