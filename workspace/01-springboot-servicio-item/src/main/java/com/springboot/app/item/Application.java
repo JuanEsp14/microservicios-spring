5c329e34726622599907336636eb60b82c63c881
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-10 09:26:53 -0300

    Se agrega el anottation para realizar la configuracion de RIBBON

--- workspace/01-springboot-servicio-item/src/main/java/com/springboot/app/item/Application.java
+++ workspace/01-springboot-servicio-item/src/main/java/com/springboot/app/item/Application.java
@@ -2,11 +2,15 @@ package com.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//Habilita la inyección de dependencias de Feign
 @SpringBootApplication
+//Habilita la inyección de dependencias de Feign
 @EnableFeignClients
+//Se anota la singular porque tenemos solamente un cliente en caso de tener
+//más se debe anotar con el annotation @RibbonClients
+@RibbonClient(name = "servicio-productos")
 public class Application {
 
 	public static void main(String[] args) {
