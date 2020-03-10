b129588832e5f5ae9a8efc2a0e40b81a35881848
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-10 10:52:07 -0300

    Se configura la dependencia Eureka en el proyecto item

--- workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/Application.java
+++ workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/Application.java
@@ -2,15 +2,21 @@ package com.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //Habilita la inyección de dependencias de Feign
 @EnableFeignClients
+
 //Se anota la singular porque tenemos solamente un cliente en caso de tener
 //más se debe anotar con el annotation @RibbonClients
-@RibbonClient(name = "servicio-productos")
+//Se elimina la configuración de Ribbon, ya que se está utilizando Eureka
+//@RibbonClient(name = "servicio-productos")
+
+//Habilitamos el cliente de Eureka en la app
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
