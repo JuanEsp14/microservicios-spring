104b30d27b51fbb5c4295a00a83d133bafbce47e
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-13 14:58:38 -0300

    Se lee la configuración en el cliente desde el servidor spring-config

--- workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/controllers/ItemController.java
+++ workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/controllers/ItemController.java
@@ -1,10 +1,17 @@
 package com.springboot.app.item.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +24,14 @@ import com.springboot.app.item.models.service.ItemService;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -46,5 +58,16 @@ public class ItemController {
 		item.setCantidad(cantidad);
 		
 		return item;
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> getConfig(@Value("${server.port}") String port){
+		
+		log.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("port", port);
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
