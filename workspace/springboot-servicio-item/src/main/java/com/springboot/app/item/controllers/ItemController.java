fa1d133dcd8d831d8080b84ef89e6468441000dc
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-13 15:29:27 -0300

    Se obtienen las properties de los archivos de configuración

--- workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/controllers/ItemController.java
+++ workspace/springboot-servicio-item/src/main/java/com/springboot/app/item/controllers/ItemController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +28,15 @@ public class ItemController {
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
 	
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
 	
+	//Las properties especificas es aconsajable ingresarlas a través del @Value ya
+	//se les puede setear un valor por defecto y no vamos a tener error de que la propertie
+	//no fue encontrada
 	@Value("${configuracion.texto}")
 	private String texto;
 	
@@ -61,13 +68,18 @@ public class ItemController {
 	}
 	
 	@GetMapping("/obtener-config")
-	public ResponseEntity<?> getConfig(@Value("${server.port}") String port){
+	public ResponseEntity<?> getConfig(){
 		
 		log.info(texto);
 		
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
-		json.put("port", port);
+		json.put("port", env.getProperty("server.port"));
+		
+		if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
