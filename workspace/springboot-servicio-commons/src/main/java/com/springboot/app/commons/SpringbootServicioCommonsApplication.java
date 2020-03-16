8daaace3c688ecc85825e8a4a8e1adfec2117202
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-16 19:57:12 -0300

    Se elimina la configuración por defecto para no incorporar una base de datos obligatoriamente

--- workspace/springboot-servicio-commons/src/main/java/com/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ workspace/springboot-servicio-commons/src/main/java/com/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,8 +1,12 @@
 package com.springboot.app.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//Deshabilitamos la autoconfiguración, para que no nos pida una base de datos obligatoria
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootServicioCommonsApplication {
 
 }
