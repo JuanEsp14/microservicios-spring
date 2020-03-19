ed875929faf2917aa5c362a1a7d8de90cacfcc65
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-19 16:15:19 -0300

    Se inyecta la librería para crear el CRUD de manera directa

--- workspace/springboot-servicio-usuarios/src/main/java/com/springboot/app/usuarios/models/repository/UsuarioDAO.java
+++ workspace/springboot-servicio-usuarios/src/main/java/com/springboot/app/usuarios/models/repository/UsuarioDAO.java
@@ -2,11 +2,16 @@ package com.springboot.app.usuarios.models.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.app.usuarios.models.entity.Usuario;
 
 //Paging And Sorting hereda todo de Repository pero le agrega la paginación y 
 //funciones de Sort
+
+//Todos los métodos del CRUD son exportados por esta anotación sin necesidad 
+//de escribirlo a mano
+@RepositoryRestResource(path="usuario")
 public interface UsuarioDAO extends PagingAndSortingRepository<Usuario, Long> {
 
 	//Ambos métodos hacen lo mismo, la primera utiliza las propiedades de 
