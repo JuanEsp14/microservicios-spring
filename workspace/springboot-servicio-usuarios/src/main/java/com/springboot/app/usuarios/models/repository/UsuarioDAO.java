ab95e0539a5dd0c1719888e6c6eda467c9c7ee40
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-19 17:26:53 -0300

    Se personalizan las rutas de los métodos personalizados

--- workspace/springboot-servicio-usuarios/src/main/java/com/springboot/app/usuarios/models/repository/UsuarioDAO.java
+++ workspace/springboot-servicio-usuarios/src/main/java/com/springboot/app/usuarios/models/repository/UsuarioDAO.java
@@ -2,7 +2,9 @@ package com.springboot.app.usuarios.models.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.springboot.app.usuarios.models.entity.Usuario;
 
@@ -19,7 +21,10 @@ public interface UsuarioDAO extends PagingAndSortingRepository<Usuario, Long> {
 	//Ambos métodos hacen lo mismo, la primera utiliza las propiedades de 
 	//Spring Data JPA y el segundo tmbién utiliza JPA pero la anotación Query
 	//con lo cual deben escribir la consulta a mano
-	public Usuario findByUsername(String username);
+	//Las anotaciones @Restresource y @Param me permiten configurar la ruta y 
+	//el nombre del parámetro a utilizar
+	@RestResource(path="buscar-username")
+	public Usuario findByUsername(@Param("username") String username);
 	
 	@Query("select u from Usuario u where u.username = ?1")
 	public Usuario obtenerPorUsername(String username);
