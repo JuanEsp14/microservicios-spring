006317718cd6301cf318156ddbf90a863bd59794
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-20 17:06:18 -0300

    Se comienza a configurar Srping Security a través de WebSecurityConfigurerAdapter

---
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.springboot.app.oauth.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService usuarioService;
+
+	
+	//Se registra el servicio en AuthenticationManager 
+	//para que tome el servicio creado.
+	//El Autowired sirve para inyectar AuthenticationManagerBuilder
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//Registramos el servico y encriptamos la contraseña
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+	}
+
+	//Registramos en Spring el BCrypt
+	@Bean
+	private BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	//Registramos el AuthenticationManager en Spring para que después lo podamos
+	//inyectar en la configuración del servidor de autorización OAuth2
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
+
+}
