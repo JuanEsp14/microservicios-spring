dc091b1672bef93b6b6f369f97ac9a1e647b8c47
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-20 16:42:40 -0300

    Se configura Feign para usar el microservicio Usuario dentro de OAuth2

--- workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -3,9 +3,15 @@ package com.springboot.app.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+
+//Habilito el cliente de Eureka
 @EnableEurekaClient
+
+//Habilito el cliente de Feign
+@EnableFeignClients
 public class SpringbootServicioOauthApplication {
 
 	public static void main(String[] args) {
---
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,16 @@
+package com.springboot.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springboot.app.commons.usuarios.models.entity.Usuario;
+
+//Se implementa Feign para consumir y utilizar el microservicio usuarios
+//Sin necesidad de tener que implementarlo
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String username);
+}
