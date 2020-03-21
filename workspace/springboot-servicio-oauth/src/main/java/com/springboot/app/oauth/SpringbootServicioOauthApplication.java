e1b876d363eb973704f18d9e7d15ad541b806709
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-21 10:29:36 -0300

    Se encriptan las contraseñan y se las agregan a los usuarios creados

--- workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -1,9 +1,12 @@
 package com.springboot.app.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 
@@ -12,10 +15,27 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //Habilito el cliente de Feign
 @EnableFeignClients
-public class SpringbootServicioOauthApplication {
 
+//La interfaz se implementa para encriptar
+//las contraseñas
+public class SpringbootServicioOauthApplication implements CommandLineRunner{
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password = "12345";
+		
+		for (int i = 0; i < 4; i++) {
+			String passwordBCrypt = passwordEncoder.encode(password);
+			System.out.println(passwordBCrypt);
+		}
+		
 	}
 
 }
--- workspace/springboot-servicio-usuarios/src/main/resources/import.sql
+++ workspace/springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('juan', '12345', 1, 'Juan', 'Espinoza', 'juanmesp@hotmail.com'); 
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'Jhon', 'Doe', 'jhon.doe@admin.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('juan', '$2a$10$tiSKnzF506UobLsrP7ciB.ZCMrKFOWIUrIAKMBtf6qEEM/0SqKsv2', 1, 'Juan', 'Espinoza', 'juanmesp@hotmail.com'); 
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$ANCwseVxvnXJG77eE4wAy.y0kSpwhPGLeybmxix.KuxVnk3IPAJq.', 1, 'Jhon', 'Doe', 'jhon.doe@admin.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
