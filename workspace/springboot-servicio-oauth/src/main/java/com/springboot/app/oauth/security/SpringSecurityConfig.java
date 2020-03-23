db7770aa5c63c8aaf567975d4d3f9b5bf34ab632
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-23 20:24:16 -0300

    Se inyecto el manejador en el logueo

--- workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/security/SpringSecurityConfig.java
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot.app.oauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,7 +15,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService usuarioService;
-
+	
+	//Se inyectael manejador de login
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
 	
 	//Se registra el servicio en AuthenticationManager 
 	//para que tome el servicio creado.
@@ -23,7 +27,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		//Registramos el servico y encriptamos la contraseña
-		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder())
+		.and().authenticationEventPublisher(eventPublisher);
+		
 	}
 
 	//Registramos en Spring el BCrypt
