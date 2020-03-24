5a7b12644f510bb867590010a462d82b7b915483
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-23 21:24:39 -0300

    Se maneja el máximos de intentos al hacer login

--- workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
+++ workspace/springboot-servicio-oauth/src/main/java/com/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -2,11 +2,17 @@ package com.springboot.app.oauth.security.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import com.springboot.app.commons.usuarios.models.entity.Usuario;
+import com.springboot.app.oauth.services.IUsuarioService;
+
+import feign.FeignException;
 
 //Se manejaran los eventos de éxito y de error al loguearse y se lanzará un evento
 @Component
@@ -14,12 +20,23 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 
 	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
 	
+	//Inyectamos el servicio de usuario
+	@Autowired
+	private IUsuarioService usuarioService;	
+	
 	@Override
 	public void publishAuthenticationSuccess(Authentication authentication) {
 		UserDetails user = (UserDetails) authentication.getPrincipal();
 		String mensaje = "Success Login: "+ user.getUsername();
 		System.out.println(mensaje);
 		log.info(mensaje);		
+		
+		Usuario usuario = this.usuarioService.findByUsername(authentication.getName());	
+		
+		if(usuario.getIntentos() != null && usuario.getIntentos() > 0) {
+			usuario.setIntentos(0);
+			this.usuarioService.update(usuario, usuario.getId());
+		}
 	}
 
 	@Override
@@ -28,6 +45,26 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		System.out.println(mensaje);
 		log.error(mensaje);		
 		
+		try {
+			Usuario usuario = this.usuarioService.findByUsername(authentication.getName());	
+			if(usuario.getIntentos() == null){
+				usuario.setIntentos(0);
+			}
+			if (usuario.getIntentos() >= 3) {
+				log.error(String.format("El usuario %s deshabilitado por máximos intentos", usuario.getUsername()));
+				usuario.setEnabled(false);
+			}	
+			
+			log.info("Intento actual es de "+ usuario.getIntentos());
+			
+			usuario.setIntentos(usuario.getIntentos()+1);			
+			
+			this.usuarioService.update(usuario, usuario.getId());
+			
+			log.info("Intento después es de "+ usuario.getIntentos());
+		}catch (FeignException e){
+			log.error(String.format("El usuario %s no existe en el sistema", authentication.getName()));	
+		}
 	}
 
 }
