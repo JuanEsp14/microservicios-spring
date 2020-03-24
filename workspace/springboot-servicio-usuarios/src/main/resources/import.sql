29be595fbefb26fad4d5a669a46ab629b4f72bcf
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-24 19:57:19 -0300

    Se comienza a configurar PostgreSQL en Usuarios

--- workspace/springboot-servicio-usuarios/src/main/resources/import.sql
+++ workspace/springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('juan', '$2a$10$Xop37FdRGYRY1T.A4h/gzubcICQEzZxjD9iV8cCsx9IUEUXsbRBXe', 1, 'Juan', 'Espinoza', 'juanmesp@hotmail.com'); 
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$g.MhXQ1COXqhyZXIS1t3lezezicJRlSzkzpXcpuks7F0ABcZbl3ci', 1, 'Jhon', 'Doe', 'jhon.doe@admin.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('juan', '$2a$10$Xop37FdRGYRY1T.A4h/gzubcICQEzZxjD9iV8cCsx9IUEUXsbRBXe', true, 'Juan', 'Espinoza', 'juanmesp@hotmail.com'); 
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$g.MhXQ1COXqhyZXIS1t3lezezicJRlSzkzpXcpuks7F0ABcZbl3ci', true, 'Jhon', 'Doe', 'jhon.doe@admin.com');
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (1,1);
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2,1);
-INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2,2);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (1,1);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2,1);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2,2);
